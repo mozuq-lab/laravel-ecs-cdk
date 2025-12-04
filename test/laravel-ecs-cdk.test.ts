240059acf05a27e624254f576a65a167c6330daf
mozuq <hello@mozuq-lab.com>
2025-12-04 22:58:54 +0900

    Initial commit

---
+++ test/laravel-ecs-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LaravelEcsCdk from '../lib/laravel-ecs-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/laravel-ecs-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LaravelEcsCdk.LaravelEcsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
